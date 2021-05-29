4e516d6d7a919bef033a3783ae7af37d13e9ac4c
develjsh <develJsh@gmail.com>
2021-05-29 10:49:51 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as XLSX from 'xlsx';
 
 function App() {
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+        // console.log(wb.SheetNames.length);
+
+        const list = ['립','블러쉬','네일','컨실러','치크','클렌저','크림','마스크','에센스']
+
+        const workbook = XLSX.utils.book_new();
+        const aloop = wb.SheetNames.map((unit, idx) => {
+          const wsname = wb.SheetNames[idx];
+          const ws = wb.Sheets[wsname];
+          const data = XLSX.utils.sheet_to_json(ws);
+
+          // 1개의 시트 전체 다 indexOf 해서 숫자 계산하기
+          const map =  {};
+          map['립'] = 0;
+          map['블러쉬'] = 0;
+          map['네일'] = 0;
+          map['컨실러'] = 0;
+          map['치크'] = 0;
+          map['클렌터'] = 0;
+          map['크림'] = 0;
+          map['마스크'] = 0;
+          map['에센스'] = 0;
+
+          for(var i=0; i<data.length; i++){
+            for(var j=0; j<list.length; j++){
+              if(data[0]["한국제품명"].indexOf(list[j]) > 0){
+                map[list[j]] =  map[list[j]] + 1;
+              }
+
+            }
+          }
+
+          for(j=0; j<list.size; j++){
+            console.log(map[list[j]]);
+          }
+          
+
+
+          const worksheet = XLSX.utils.json_to_sheet(data);
+          XLSX.utils.book_append_sheet(workbook, worksheet, wsname);
+
+          return unit;
+        });
+        XLSX.writeFile(wb, 'Test.xlsx');
+
+        // const wsname = wb.SheetNames[0];
+        // const ws = wb.Sheets[wsname];
+        // const data = XLSX.utils.sheet_to_json(ws);
+
+        // resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      const ws = XLSX.utils.json_to_sheet(d);
+
+      const wb = XLSX.utils.book_new();
+
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+      XLSX.writeFile(wb, 'Test.xlsx');
+
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
     </div>
   );
 }
