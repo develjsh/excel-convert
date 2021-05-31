86e67ca3e349e39c78fb39df5ede0a4c0c9c95ad
develjsh <develJsh@gmail.com>
2021-06-01 01:46:34 +0900

    21-06-01 need to count num

--- src/App.js
+++ src/App.js
@@ -16,15 +16,33 @@ function App() {
         workbook.SheetNames.map((unit, idx) => {
           const wsname = workbook.SheetNames[idx];
           data = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]);
-          const lib = data.filter(n => n.한국제품명.includes('립'));
-          const blush = data.filter(n => n.한국제품명.includes('블러쉬'));
-          const nail = data.filter(n => n.한국제품명.includes('네일'));
-          const consiler = data.filter(n => n.한국제품명.includes('컨실러'));
-          const chick = data.filter(n => n.한국제품명.includes('치크'));
-          const cleanger = data.filter(n => n.한국제품명.includes('클렌저'));
-          const cream = data.filter(n => n.한국제품명.includes('크림'));
-          const mask = data.filter(n => n.한국제품명.includes('마스크'));
-          const essense = data.filter(n => n.한국제품명.includes('에센스'));
+          const lib = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('립')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const blush = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('블러쉬')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const nail = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('네일')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const consiler = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('컨실러')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const chick = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('치크')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const cleanger = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('클랜저')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const cream = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('크림')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const mask = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('마스크')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
+          const essense = data.reduce(function (accumulator, n){
+            return n.한국제품명.includes('에센스')? accumulator + n.수량 : accumulator + 0  
+          }, 0);
           // const sheetData = [
           //   {'립': lib.length}, 
           //   {'블러쉬' : blush.length},
@@ -41,15 +59,14 @@ function App() {
           const myheader = ['이름', '갯수'];
           // sheetData.push(['립', lib.length]);
           // sheetData.push(['블러쉬', blush.length]);
-          // sheetData.push(['네일', nail.length]);
-          // sheetData.push(['컨실러', consiler.length]);
-          // sheetData.push(['치크', chick.length]);
-          // sheetData.push(['클렌저', cleanger.length]);
-          // sheetData.push(['크림', cream.length]);
-          // sheetData.push(['마스크', mask.length]);
-          // sheetData.push(['에센스', essense.length]);
+          // sheetData.push(['컨실러', consiler]);
+          // sheetData.push(['치크', chick]);
+          // sheetData.push(['클렌저', cleanger]);
+          // sheetData.push(['크림', cream]);
+          // sheetData.push(['마스크', mask]);
+          // sheetData.push(['에센스', essense]);
           const names = ['립', '블러쉬', '네일', '컨실러', '치크', '클렌저', '크림', '마스크', '에센스'];
-          const num = [lib.length, blush.length, nail.length, consiler.length, chick.length, cleanger.length, cream.length, mask.length, essense.length];
+          const num = [lib, blush, nail, consiler, chick, cleanger, cream, mask, essense];
           const sheetData = [];
           names.map((unit, idx) =>{
             sheetData.push({"이름" : names[idx], "갯수" : num[idx]});
